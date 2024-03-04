896df5cd480b9104e3e01e36ae7a023203b68ed6
jimbex <jimohbasit4@gmail.com>
2024-03-04 17:48:59 +0000

    Create index.js

---
+++ js-files/index.js
@@ -0,0 +1,5 @@
+function login() {
+            // Add your authentication logic here
+            // For simplicity, this example always redirects to the control page
+            window.location.href = 'find.html';
+        }

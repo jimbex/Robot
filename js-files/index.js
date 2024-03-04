2d6a6532563ee8edf6e186328e784a0fc887a2e5
jimbex <jimohbasit4@gmail.com>
2024-03-04 18:32:54 +0000

    Update index.js

--- js-files/index.js
+++ js-files/index.js
@@ -2,16 +2,9 @@ function login() {
     const enteredPassword = document.getElementById('password').value;
 
     // Simulate checking entered password against stored data
-    fetch('json-file/users.json')
-        .then(response => response.json())
-        .then(data => {
-            const storedPassword = data.password;
-
-            if (enteredPassword === storedPassword) {
-                window.location.href="find.html";
-            } else {
-                alert('Invalid password. Please try again.');
-            }
-        })
-        .catch(error => console.error('Error:', error));
+    if (enteredPassword === '1234') {
+        window.location.href="find.html";
+    } else {
+        alert('Invalid password. Please try again.');
+    }
 }
